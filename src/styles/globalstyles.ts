a06b22239a90ae200e7e4165f3ac23c405b9ebe0
JaeilJo <whwodlf97@gmail.com>
2023-05-21 14:43:20 +0900

    first commit

---
+++ src/styles/globalstyles.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+import GlobalStyle from '@/styles/globalstyles';
+const GlobalStyles = createGlobalStyle`
+
+  html,
+  body {
+    font-size: 62.5%;
+    padding: 0;
+    margin: 0;
+    font-family: 'Oswald', sans-serif;
+    scroll-behavior: smooth;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+export default GlobalStyles;
