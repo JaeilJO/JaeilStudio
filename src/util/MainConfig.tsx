b6a9539579cd6c530c29e28f00dfe84f0baa829f
JaeilJO <whwodlf97@gmail.com>
2023-05-24 17:30:27 +0900

    refactor : add content in item

--- src/util/MainConfig.tsx
+++ src/util/MainConfig.tsx
@@ -1,28 +1,43 @@
-export const MainConfig = {
+import { SectionProps } from "@/components/Section";
+
+interface SectionPropsWithKey extends SectionProps {
+  key: number;
+}
+
+export interface MainConfigItemsType {
+  items: SectionPropsWithKey[];
+}
+
+const MainConfig: MainConfigItemsType = {
   items: [
     {
       key: 1,
       id: "about",
       type: "white",
+      content: <></>,
       option: { left: false, right: false },
     },
     {
       key: 2,
       id: "skills",
       type: "black",
+      content: <></>,
       option: { left: false, right: false },
     },
     {
       key: 3,
       id: "works",
       type: "white",
+      content: <></>,
       option: { left: false, right: false },
     },
     {
       key: 4,
       id: "contact",
-      type: "white",
+      type: "black",
+      content: <></>,
       option: { left: false, right: false },
     },
   ],
 };
+export default MainConfig;
