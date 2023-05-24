62f7c99cf190e13f3e93bc828fd13bf58edd169c
JaeilJO <whwodlf97@gmail.com>
2023-05-24 15:38:05 +0900

    refactor : I added "pointer-events:none" to the Logo

---
+++ src/components/atoms/Logo/index.styled.ts
@@ -0,0 +1,25 @@
+import { FontSizeType, FontWeightType } from "@/styles/theme";
+import { styled } from "styled-components";
+
+interface LogoStyleProp {
+  font_size: keyof FontSizeType;
+  font_weight: keyof FontWeightType;
+}
+
+const S = {
+  Logo: styled.div<LogoStyleProp>`
+    font-size: ${({ theme, font_size }) => theme.font_size.large[font_size]};
+    font-weight: ${({ theme, font_weight }) => theme.font_weight[font_weight]};
+    transition: 280ms;
+    pointer-events: none;
+    @media ${({ theme }) => theme.media_query.medium} {
+      font-size: ${({ theme, font_size }) => theme.font_size.medium[font_size]};
+    }
+
+    @media ${({ theme }) => theme.media_query.small} {
+      font-size: ${({ theme, font_size }) => theme.font_size.small[font_size]};
+    }
+  `,
+};
+
+export default S;
